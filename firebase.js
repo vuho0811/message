e8639c6b1e1b9042faec0650f4e8a3ec80cfac41
vuho0811 <hovu1@msu.edu>
2021-06-09 13:35:24 +0700

    asd

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 import 'firebase/storage';  // <----
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoAX8bjTtS27YlHEh-miFpgL51UjKJLew",
-  authDomain: "myapp-7b5f9.firebaseapp.com",
-  projectId: "myapp-7b5f9",
-  storageBucket: "myapp-7b5f9.appspot.com",
-  messagingSenderId: "529318295078",
-  appId: "1:529318295078:web:7929e9b3593e926c7fd8e7",
-  measurementId: "G-8QCMZ1TJX6"
+  apiKey: "AIzaSyByFp24EkX4t3NBAoX-zbhgJ4hGE5Q_FKs",
+  authDomain: "messenger-cd1d4.firebaseapp.com",
+  projectId: "messenger-cd1d4",
+  storageBucket: "messenger-cd1d4.appspot.com",
+  messagingSenderId: "840287891445",
+  appId: "1:840287891445:web:0fd6621b682d5e971fbabc",
+  measurementId: "G-F69GR6KE6P"
 };
 
 //   const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
